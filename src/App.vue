79cd084d84a2d0628675b0cb107d5fa83f23cac2
Arthur Kachmazov <boss.kachmaz@mail.ru>
2022-04-04 15:10:14 +0300

    New font Taverna

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,21 @@ export default {};
 </script>
 
 <style>
+@font-face {
+  font-family: "Taverna";
+  src: local("Taverna"),
+   url(./fonts/Taverna.ttf) format("truetype");
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Taverna, sans-serif;
+  font-weight: 400;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
