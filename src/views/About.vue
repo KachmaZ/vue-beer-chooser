97eacead650150df154951cf83ac49e5c15f96c9
Arthur Kachmazov <boss.kachmaz@mail.ru>
2022-04-11 19:58:52 +0300

    About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,16 +1,38 @@
 // About view component
 <template>
-    <div>
-        About page
-    </div>
+  <h1 class="about-title">About beer</h1>
+  <div class="beer-info">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti vitae
+    dicta voluptates aperiam adipisci culpa eum commodi ex tempora itaque,
+    molestiae aut quisquam quas iusto consequuntur autem ratione, vero nobis,
+    suscipit minus tenetur quo laudantium. Temporibus, eos pariatur error non
+    impedit rem. Nam exercitationem, ratione culpa deleniti, rerum, excepturi
+    dolorem similique accusamus autem commodi et quae voluptatem. Eos mollitia
+    iure a suscipit molestias, voluptatum, officiis veritatis laboriosam sit
+    excepturi, dolorum modi natus asperiores tempora accusantium similique quae
+    architecto dolorem non eius magnam velit rem. Culpa similique nobis quod ad
+    quidem quisquam nulla, corporis nam provident repudiandae asperiores
+    reiciendis architecto voluptatem.
+  </div>
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
 
 <style lang="scss" scoped>
+.about-title {
+   margin-top: 50px;
 
+   font-size: 52px;
+   text-align: center;
+}
+
+.beer-info {
+    width: 80%;
+
+    margin: 50px 10% 0;
+
+    text-align: justify;
+}
 </style>
